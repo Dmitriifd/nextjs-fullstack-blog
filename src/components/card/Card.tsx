f69d6294afbdda1d6df58a2b634ec81dfd26d6a1
Dmitriifd <idimka9@gmail.com>
2023-09-10 19:35:15 +0500

    Card, CardList & Menu

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,13 +1,33 @@
-import React from "react";
+import Image from 'next/image';
 import styles from './card.module.css';
+import Link from 'next/link';
 
 const Card = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src='/p1.jpeg' alt='' fill className={styles.image} />
+      </div>
 
+      <div className={styles.textContainer}>
+        <div className={styles.detail}>
+          <span className={styles.date}>10.09.2023 - </span>
+          <span className={styles.category}>CULTURE</span>
+        </div>
+        <Link href={`/posts/temp`}>
+          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+        </Link>
+        <p className={styles.desc}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia officiis, architecto vero eum repellat magnam accusamus. Quaerat soluta dignissimos aspernatur, labore beatae non perferendis, rem officia fugiat minima aut excepturi.</p>
+        {/* <div
+          className={styles.desc}
+          dangerouslySetInnerHTML={{ __html: item?.desc.substring(0, 60) }}
+        /> */}
+        <Link href={`/posts/temp`} className={styles.link}>
+          Read More
+        </Link>
+      </div>
     </div>
   );
 };
 
 export default Card;
-
--- src/components/cardList/CardList.tsx
+++ src/components/cardList/CardList.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from './cardList.module.css';
+import Card from "../card/Card";
 
-const CardList = () => {
+const CardList = ({page = 1}: {page: number}) => {
   return (
     <div className={styles.container}>
-
+      <h1 className={styles.title}>Recent Posts</h1>
+      <div className={styles.posts}>
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 };
