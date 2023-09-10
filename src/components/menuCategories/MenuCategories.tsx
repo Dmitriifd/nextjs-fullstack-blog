e6a9c070de4190f821ecdc835c2aaead143c3839
Dmitriifd <idimka9@gmail.com>
2023-09-10 15:02:26 +0500

    заглушки для компонентов

---
+++ src/components/menuCategories/MenuCategories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from './menuCategories.module.css';
+
+const MenuCategories = () => {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+};
+
+export default MenuCategories;
